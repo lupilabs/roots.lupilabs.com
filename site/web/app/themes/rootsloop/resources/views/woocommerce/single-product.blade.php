34c6c1309e99f69a37ece9158416b63d23940f6f
lupilabs <wolf@lupilabs.com>
2022-06-03 02:16:21 +0200

    WordPress 6.0, Sage 10.1.7 with 6.5.1 WooCommerce generoi/sage-woocommerce

--- site/web/app/themes/rootsloop/resources/views/woocommerce/single-product.blade.php
+++ site/web/app/themes/rootsloop/resources/views/woocommerce/single-product.blade.php
@@ -17,21 +17,21 @@ the readme will list any important changes.
 @extends('layouts.app')
 
 @section('content')
-@php
-do_action('get_header', 'shop');
-do_action('woocommerce_before_main_content');
-@endphp
+  @php
+    do_action('get_header', 'shop');
+    do_action('woocommerce_before_main_content');
+  @endphp
 
-@while(have_posts())
-@php
-the_post();
-wc_get_template_part('content', 'single-product');
-@endphp
-@endwhile
+  @while(have_posts())
+    @php
+      the_post();
+      wc_get_template_part('content', 'single-product');
+    @endphp
+  @endwhile
 
-@php
-do_action('woocommerce_after_main_content');
-do_action('get_sidebar', 'shop');
-do_action('get_footer', 'shop');
-@endphp
+  @php
+    do_action('woocommerce_after_main_content');
+    do_action('get_sidebar', 'shop');
+    do_action('get_footer', 'shop');
+  @endphp
 @endsection
